faa83542f6ed572b738d0cfcbf9c398393094ee1
Vinay Sharma <vinaysharma7811@gmail.com>
2020-12-04 21:39:22 +0530

    fix: package json import error in messages file

--- src/messages/index.ts
+++ src/messages/index.ts
@@ -1,12 +1,14 @@
 import chalk from "chalk";
-const { name, bugs } = require('../package.json');
+
+const name = '@react/cli';
+const repo = 'https://github.com/vinaysharma14/react-cli/issues'
 
 const messages = {
   welcome: `Welcome to ${chalk.bold(name)}! A zero config lightweight CLI for automating React boilerplate setup.`,
   walkThrough: 'This utility will walk you through steps to configure a React boilerplate as per your requirements.',
   complete: 'Setup is complete! You can check app in',
   thanks: `Thanks for using ${chalk.bold(name)}. Happy Coding! </>`,
-  raiseIssue: `Raise an issue here: ${chalk.green(bugs.url)}`,
+  raiseIssue: `Raise an issue here: ${chalk.green(repo)}`,
 }
 
 const features = [
