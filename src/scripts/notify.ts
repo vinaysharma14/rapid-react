67c153d11c1479f30a7f9bcbacf75d461dc24b3f
Vinay Sharma <vinaysharma7811@gmail.com>
2021-02-08 22:51:10 +0530

    fix: notification sound directory path

--- src/scripts/notify.ts
+++ src/scripts/notify.ts
@@ -1,9 +1,7 @@
 const player = require('play-sound')();
 
 export const notify = () => {
-  const [dir] = __dirname.split('rapid-react/')[1].split('/');
-
-  player.play(`${dir}/assets/notification.mp3`, function(error: string) {
+  player.play(`${__dirname.replace('scripts', 'assets/notification.mp3')}`, function(error: string) {
     error && console.error(error);
   });
 };
